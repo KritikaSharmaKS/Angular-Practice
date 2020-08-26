f70c038c3e32dbaba49790e579526784ae6edc4b
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-08-25 21:23:54 -0400

    add import and declaration for new component

--- Section-1 Getting Started/my-first-app/src/app/app.module.ts
+++ Section-1 Getting Started/my-first-app/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ServerComponent } from './server/server.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
