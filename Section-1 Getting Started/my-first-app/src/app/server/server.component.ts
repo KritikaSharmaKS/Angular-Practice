c1752e4a0021bf5e34795e9bf68008aaa07896f8
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-08-30 19:06:05 -0400

    complete assignment 2

--- Section-1 Getting Started/my-first-app/src/app/server/server.component.ts
+++ Section-1 Getting Started/my-first-app/src/app/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
     selector: 'app-server',
@@ -7,9 +8,15 @@ import { Component } from '@angular/core';
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = "online"
+    username = "type your name here";
 
     getServerStatus() {
         return this.serverStatus;
     }
+
+    onButtonClick() {
+        return this.username = '';
+    }
+
 
 }
